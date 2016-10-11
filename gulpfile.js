18d08c16e06a52b0f43c26e7e561c2c28e022e03
hagishi <hagishi.c@gmail.com>
2016-10-12 06:03:03 +0900

    add gulp task docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('sync', function() {
   browserSync.init({
     server: {
       baseDir: "./"
